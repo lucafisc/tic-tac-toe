045bd99f6bff8015a1100d7dc0d0f2a382d0c649
lucafisc <lucafischer@fastmail.com>
2022-06-20 11:26:15 +0200

    Add scores and rounds

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const playerFactory = (name, marker, score) => {
   return { name, marker, score };
 };
-
 const human = playerFactory("human", "x", 0);
 const cpu = playerFactory("cpu", "o", 0);
 
@@ -12,27 +11,40 @@ const gameboard = (() => {
   //event listener for cells
   for (let cell of _cells) {
     cell.addEventListener("click", function (e) {
-        console.log(gameControl.isWinner);
 
-      if (this.textContent !== "" || gameControl.isWinner) return; // || gameControl.whoseTurn() === cpu
+      if (this.classList.contains("x") || this.classList.contains("o") || gameControl.isThereWinner()) return; // || gameControl.whoseTurn() === cpu
       _render(this, gameControl.whoseTurn().marker);
       gameControl.gameRound();
     });
   }
 
   function _render(cell, marker) {
-    cell.classList.add("marked");
-    cell.textContent = marker;
+    cell.classList.add(marker);
+    console.log(cell.id);
+    console.log(marker);
     board[cell.id] = marker;
+    console.log(board);
   }
 
   function grayOut(winnerArray) {
     for (let i = 0; i < _cells.length; i++) {
+        _cells[i].classList.add("new-round");
       if (!winnerArray.includes(i)) {
         _cells[i].classList.add("grayed");
       }
     }
+    setTimeout(function(){
+        for (let i = 0; i < _cells.length; i++)  {
+            _cells[i].classList.remove("new-round");
+            _cells[i].classList.remove("grayed");
+            _cells[i].classList.remove("x");
+            _cells[i].classList.remove("o");
+        }
+},3000);
+
+board = ["", "", "", "", "", "", "", "", ""];
   }
+
 
   return {
     board: board,
@@ -44,7 +56,13 @@ const gameControl = (() => {
   let indexes = [];
   let winnerArray;
   let _currentPlayer = human;
-  let isWinner = false;
+  let _isWinner = false;
+  let _boardContainer = document.querySelector(".board");
+  let _playerPoints = 0;
+  let _cpuPoints = 0;
+  let _playerScore = document.querySelector('[data-index="human"]');
+  let _cpuScore = document.querySelector('[data-index="cpu"]');
+
 
   const _winConditions = [
     [0, 1, 2],
@@ -61,9 +79,22 @@ const gameControl = (() => {
     _currentPlayer === human
       ? (_currentPlayer = cpu)
       : (_currentPlayer = human);
+    _boardContainer.classList.toggle("human");
+    _boardContainer.classList.toggle("cpu");
+  }
+
+  function _updatePoints(winner) {
+    winner === human
+      ? _playerPoints++
+      : _cpuPoints++;
+    
+      _playerScore.textContent = _playerPoints;
+      _cpuScore.textContent = _cpuPoints;
   }
 
   function _checkWin(marker, array, winCondition) {
+    console.log(array);
+
     //check index of markers
     indexes = [];
     for (let i = 0; i < array.length; i++) {
@@ -74,33 +105,44 @@ const gameControl = (() => {
     //check if matches winner array
     for (let i = 0; i < winCondition.length; i++) {
       if (winCondition[i].every((j) => indexes.includes(j))) {
-        isWinner = true;
+        _isWinner = true;
         winnerArray = winCondition[i];
       }
     }
-    return isWinner;
   }
 
-  function _gameOver() {}
+  function _gameOver() {
+      _isWinner = false;
+indexes = [];
+winnerArray = [];
+gameboard.board = ["", "", "", "", "", "", "", "", ""];
+
+  }
 
   function whoseTurn() {
     return _currentPlayer;
   }
 
   function gameRound() {
-    if (_checkWin(whoseTurn().marker, gameboard.board, _winConditions)) {
-      _gameOver();
+    _checkWin(whoseTurn().marker, gameboard.board, _winConditions)
+    if (_isWinner) {
       title.animateGameOver();
       gameboard.grayOut(winnerArray);
+      _updatePoints(_currentPlayer);
+      _gameOver();
     } else {
       title.animateTurn(_currentPlayer);
       _switchPlayer();
     }
   }
+
+  function isThereWinner() {
+      return _isWinner;
+  }
   return {
     gameRound: gameRound,
     whoseTurn: whoseTurn,
-    isWinner: isWinner
+    isThereWinner: isThereWinner,
   };
 })();
 
@@ -139,3 +181,4 @@ const title = (() => {
     animateGameOver: animateGameOver,
   };
 })();
+
